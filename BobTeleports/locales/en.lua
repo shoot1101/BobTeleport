067904e23b26b987cdd0b8ffe11c3a91790d6825
Bob's & Co <33040669+shoot1101@users.noreply.github.com>
2024-11-27 11:46:36 +0100

    Update en.lua

--- BobTeleports/locales/en.lua
+++ BobTeleports/locales/en.lua
@@ -1,12 +1,13 @@
-Locales['en'] = {
-	['notif1'] = 'Notification',
-	['notif2'] = 'Notification',
-	['notif3'] = 'Notification',
-	['tpc'] = 'Teleportation in progress',
-	['tpr'] = 'Teleportation successful to ',
-	['open'] = '[E] - Open elevator',
-	['impv'] = 'Impossible by car',
-	['impp'] = 'Impossible by passenger',
-	['cld'] = 'Please wait before teleporting again',
+Locales = Locales or {}  
 
+Locales['en'] = {
+	['notif1'] 						= 'Notification',
+	['notif2'] 						= 'Notification',
+	['notif3'] 						= 'Notification',
+	['tpc'] 						= 'Teleportation in progress',
+	['tpr'] 					    = 'Teleportation successful to ',
+	['open'] 						= '[E] - Open elevator',
+	['impv'] 						= 'Impossible by car',
+	['impp'] 						= 'Impossible by passenger',
+	['cld'] 						= 'Please wait before teleporting again',
 }
